1eb01283b05fed7e49df6090fd26333ce2972207
HaidarShah208 <alhi7896542@gmail.com>
2024-10-11 10:42:48 +0500

    fix order controller

--- src/pages/allOrders/AllOrder.jsx
+++ src/pages/allOrders/AllOrder.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import instance from '../../utils/instance';
 
 function AllOrder() {
+  const [books, setBooks] = useState("");
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const response = await instance.get(`/getAllOrders`);
+        setBooks(response.data.data);
+        console.log(response)
+      } catch (error) {
+        console.error("Error fetching books:", error.response.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchBooks();
+  }, []);
   return (
     <div>
-      all orders
+      {books.map(()=>{
+         
+      })}
     </div>
   )
 }
